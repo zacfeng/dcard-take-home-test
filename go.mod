9063d4f66f20b2d3ed0f43f7e7e7c73cb468c731
Zac Chien <fengying0709@gmail.com>
2020-11-28 01:18:06 +0800

    feat: show rate count in response

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/zacfeng/dcard-take-home-test
 
 go 1.13
 
-require github.com/gin-gonic/gin v1.6.3
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/ulule/limiter v2.2.2+incompatible
+)
--- main.go
+++ main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
-	"github.com/zacfeng/dcard-take-home-test/routers"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/zacfeng/dcard-take-home-test/middlewares"
 )
 
+// SetupRouter defines api routes
+func SetupRouter() *gin.Engine {
+	router := gin.Default()
+	router.Use(gin.Recovery())
+	router.Use(middlewares.RateLimit())
+
+	router.GET("/rate", func(c *gin.Context) {
+		c.String(http.StatusOK, c.MustGet("rate").(string))
+	})
+
+	return router
+}
+
 func main() {
-	router := routers.SetupRouter()
-	router.Run()
+	SetupRouter().Run()
 
 }
