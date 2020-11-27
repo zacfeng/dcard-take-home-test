d9cb9117a71cb7fd9d53a6b7667d13a9bf20a863
Zac Chien <fengying0709@gmail.com>
2020-11-28 01:32:15 +0800

    feat: add rate limit middleware

---
+++ middlewares/rate_limit.go
@@ -0,0 +1,58 @@
+package middlewares
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis"
+)
+
+// RateLimit limit 60 rpm request
+func RateLimit() gin.HandlerFunc {
+
+	redisURL := "redis://localhost:6379/0"
+	option, err := redis.ParseURL(redisURL)
+
+	if err != nil {
+		panic(err)
+	}
+
+	client := redis.NewClient(option)
+
+	return func(c *gin.Context) {
+		key := c.ClientIP()
+
+		cnt, err := client.Incr(key).Result()
+		if err == nil && cnt == 1 { // the key was just created
+			err = client.ExpireAt(key, time.Now().Add(1*time.Minute)).Err()
+		}
+
+		// reach reate limit
+		if cnt > 60 {
+			c.String(http.StatusOK, "Error")
+			c.Abort()
+			return
+		}
+
+		if err != nil {
+			c.AbortWithStatus(http.StatusServiceUnavailable)
+		}
+
+		ttl, ttlErr := client.TTL(key).Result()
+
+		if ttlErr != nil {
+			c.AbortWithStatus(http.StatusServiceUnavailable)
+		}
+
+		c.Header("X-RateLimit-Limit", strconv.FormatInt(60, 10))
+		c.Header("X-RateLimit-Remaining", strconv.FormatInt(60-cnt, 10))
+		c.Header("X-RateLimit-Reset", fmt.Sprintf("%.0f", ttl.Seconds()))
+
+		c.Set("rate", strconv.FormatInt(cnt, 10))
+
+		c.Next()
+	}
+}
